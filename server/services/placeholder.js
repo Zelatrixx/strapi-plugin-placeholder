a182b214609e6d41e4eb8544f78a24bb59302caf
Zelatrixx <102881739+Zelatrixx@users.noreply.github.com>
2024-11-03 20:17:37 +0100

    Update placeholder.js
    Rewritten for plaiceholder@3.0.0

--- server/services/placeholder.js
+++ server/services/placeholder.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const { getPlaiceholder } = require('plaiceholder');
 const { getService } = require('../utils');
+const fs = require('fs').promises;
+const path = require('path');
 
 module.exports = ({ strapi }) => ({
   /**
@@ -9,11 +10,14 @@ module.exports = ({ strapi }) => ({
    * @param {string} url a local or remote image URL to generate a placeholder for
    * @returns {Promise<string>} a base64 encoded placeholder image
    */
-
   async generate(url) {
     try {
+      const { getPlaiceholder } = await import('plaiceholder');
       const settings = getService(strapi, 'settings').get();
-      const { base64 } = await getPlaiceholder(url, settings);
+      const relativePath = url.replace(/^\/uploads/, '');
+      const imagePath = path.join(strapi.dirs.static.public, 'uploads', relativePath);
+      const buffer = await fs.readFile(imagePath);
+      const { base64 } = await getPlaiceholder(buffer, settings);
       return base64;
     } catch (e) {
       strapi.log.error(e);
